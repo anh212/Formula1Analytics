f3ab9ce07dcb6a70aa2b3d119795cfb02ad3b83d
Andrew Ha <andrewha428@gmail.com>
2020-05-22 23:02:55 -0400

    Added initial chart graph

--- front-end/formula1analytics/src/components/Graph/Graph.js
+++ front-end/formula1analytics/src/components/Graph/Graph.js
@@ -1,19 +1,61 @@
-import React from 'react';
-import { Layout } from 'antd';
+import React, { useEffect, useRef} from 'react';
+import Chart from "chart.js";
 import './Graph.css';
 import 'antd/dist/antd.css';
-import GraphOptions from '../GraphOptions/GraphOptions';
-
-const { Content } = Layout;
 
 function Graph() {
+  //Chart object
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    const myChartRef = chartRef.current.getContext("2d");
+
+    new Chart(myChartRef, {
+      type: 'bar',
+      data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+  });
+  });
+
   return (
     <div className="graph-layout">
-      <Content style={{ padding: '0 0px' }}>
-        <div className="site-layout-content">
-          <GraphOptions />
-        </div>
-      </Content>
+      <div className="graphContainer">
+        <canvas
+          id="myChart"
+          ref={chartRef}
+        />
+      </div>
     </div>
   );
 }
