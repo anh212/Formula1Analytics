83c8593ee35325c17f472d770ffc05d151d3ad5d
Andrew Ha <andrewha428@gmail.com>
2020-05-22 23:06:07 -0400

    Modified App.js to utilize ContentSection

--- front-end/formula1analytics/src/App.js
+++ front-end/formula1analytics/src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 import Navigation from './components/Navigation/Navigation';
-import Graph from './components/Graph/Graph';
 import MainFooter from './components/Footer/MainFooter'
 import { Layout } from 'antd';
+import ContentSection from './components/ContentSection/ContentSection';
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <Navigation />
-        <Graph />
+        <ContentSection />
         <MainFooter />
       </Layout>
     </div>
