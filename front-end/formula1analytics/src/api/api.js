0c9237796a205c05de6ede592cd4b630316f6e1d
Andrew Ha <andrewha428@gmail.com>
2020-06-01 23:47:43 -0400

    Processed data from API to send to UI

--- front-end/formula1analytics/src/api/api.js
+++ front-end/formula1analytics/src/api/api.js
@@ -1,52 +1,77 @@
 let apiURL =  "http://ergast.com/api/f1";
 
 /**Get get all seasons/years */
-function getYears() {
+export async function getYears() {
+    console.log("Getting seasons from API");
     const response = await fetch(apiURL + "/seasons.json?limit=100");
+    let data = await response.json()
 
-    return response;
+    //Remove extraneous data such as wiki links
+    let seasons = data.MRData.SeasonTable.Seasons.map((value) => {
+        return value.season
+    });
+
+    return seasons;
 }
-
 
 /**Getting information for drivers */
-function getDriverInfo(season) {
-    const response = await fetch(apiURL + "/" + season + "/drivers");
+export async function getDriverInfo(season) {
+    const response = await fetch(apiURL + "/" + season + "/drivers.json");
+    console.log(response);
+    let data = await response.json();
 
-    return response;
+    console.log(data);
+
+    //Only need data for driverId, givenName, and familyName
+    let drivers = data.MRData.DriverTable.Drivers.map((value) => {
+        return {
+            driverId: value.driverId,
+            givenName: value.givenName,
+            familyName: value.familyName
+        }
+    });
+    console.log(drivers);
+    return drivers;
 }
 
-function getDriverWins(driver, season) {
+export async function getDriverWins(driver, season) {
     const response = await fetch(apiURL + "/" + season + "/drivers/" + driver + "/results/1.json?limit=100");
 
     return response;
 }
 
-function getPolePositions(driver, season) {
+export async function getPolePositions(driver, season) {
     const response = await fetch(apiURL + "/" + season + "/drivers/" + driver + "/grid/1/results.json?limit=100");
 
     return response;
 }
 
-function getFastestLaps(driver, season) {
+export async function getFastestLaps(driver, season) {
     const response  = await fetch(apiURL + "/" + season + "/drivers/" + driver + "/fastest/1/results.json?limit=100");
 
     return response
 }
 
-function getLapsLed(driver, season, round) {
+export async function getLapsLed(driver, season, round) {
     const response = await fetch(apiURL + "/" + season + "/" + round + "/drivers/" + driver + "/laps.json?limit=1000");
 
     return response;
 }
 
-function getLapsLedAllDrivers(season, round) {
+export async function getLapsLedAllDrivers(season, round) {
     const response = await fetch(apiURL + "/" + season + "/" + round + "/laps.json?limit=1000");
 
     return response;
 }
 
 /**Get constructor information */
-function getAllConstructorWinsAndPoints(season) {
+export async function getConstructorInfo(season) {
+    const response = await fetch(apiURL + "/" + season + "/constructors.json");
+
+    return response;
+}
+
+export async function getAllConstructorWinsAndPoints(season) {
     const response = await fetch(apiURL + "/" + season + "/constructorStandings.json");
 
     return response;
