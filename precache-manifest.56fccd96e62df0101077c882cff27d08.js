cf33c4ab34e241141af9fd55faca1be1e3239952
Andrew Ha <andrewha428@gmail.com>
2020-06-14 23:18:12 -0400

    Updates

---
+++ precache-manifest.56fccd96e62df0101077c882cff27d08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96485dced9cbd29e50e2e3fdc75d67c3",
+    "url": "/Formula1Analytics/index.html"
+  },
+  {
+    "revision": "f8197d0701e8fda16639",
+    "url": "/Formula1Analytics/static/css/2.dafc00e9.chunk.css"
+  },
+  {
+    "revision": "fc70ebfa587239bcbbfa",
+    "url": "/Formula1Analytics/static/css/main.c51f259d.chunk.css"
+  },
+  {
+    "revision": "f8197d0701e8fda16639",
+    "url": "/Formula1Analytics/static/js/2.9042a512.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/Formula1Analytics/static/js/2.9042a512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc70ebfa587239bcbbfa",
+    "url": "/Formula1Analytics/static/js/main.1f5289c3.chunk.js"
+  },
+  {
+    "revision": "0999668ad70fb471cdf4",
+    "url": "/Formula1Analytics/static/js/runtime-main.2266e84f.js"
+  }
+]);
